9c01292543821bce945f2af8bcf307b5e4fda3a3
ALBooysen78 <albooysen78@gmail.com>
2022-10-11 10:11:03 +0200

    Changed some names

---
+++ Semester-2-Poe-Part-2/Semester 2 POE Part 2/Semester 2 POE Part 1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Semester_2_POE_Part_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.downButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.mapDisplayTextBox = new System.Windows.Forms.TextBox();
+            this.playerStatsLabel = new System.Windows.Forms.Label();
+            this.EnemyStatsTextbox = new System.Windows.Forms.RichTextBox();
+            this.SelectEnemyDropDownList = new System.Windows.Forms.ComboBox();
+            this.attackEnemyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // upButton
+            // 
+            this.upButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.upButton.Location = new System.Drawing.Point(412, 237);
+            this.upButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(67, 75);
+            this.upButton.TabIndex = 0;
+            this.upButton.Text = "Up";
+            this.upButton.UseVisualStyleBackColor = true;
+            this.upButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.leftButton.Location = new System.Drawing.Point(332, 331);
+            this.leftButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(67, 75);
+            this.leftButton.TabIndex = 1;
+            this.leftButton.Text = "Left";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // downButton
+            // 
+            this.downButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.downButton.Location = new System.Drawing.Point(412, 331);
+            this.downButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(67, 75);
+            this.downButton.TabIndex = 2;
+            this.downButton.Text = "Down";
+            this.downButton.UseVisualStyleBackColor = true;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rightButton.Location = new System.Drawing.Point(493, 331);
+            this.rightButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(67, 75);
+            this.rightButton.TabIndex = 3;
+            this.rightButton.Text = "Right";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // mapDisplayTextBox
+            // 
+            this.mapDisplayTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mapDisplayTextBox.Font = new System.Drawing.Font("Monospac821 BT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mapDisplayTextBox.Location = new System.Drawing.Point(70, 42);
+            this.mapDisplayTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mapDisplayTextBox.Multiline = true;
+            this.mapDisplayTextBox.Name = "mapDisplayTextBox";
+            this.mapDisplayTextBox.ReadOnly = true;
+            this.mapDisplayTextBox.Size = new System.Drawing.Size(240, 361);
+            this.mapDisplayTextBox.TabIndex = 4;
+            // 
+            // playerStatsLabel
+            // 
+            this.playerStatsLabel.AutoSize = true;
+            this.playerStatsLabel.Location = new System.Drawing.Point(363, 42);
+            this.playerStatsLabel.Name = "playerStatsLabel";
+            this.playerStatsLabel.Size = new System.Drawing.Size(176, 17);
+            this.playerStatsLabel.TabIndex = 5;
+            this.playerStatsLabel.Text = "Player stats comes here wow";
+            // 
+            // EnemyStatsTextbox
+            // 
+            this.EnemyStatsTextbox.Location = new System.Drawing.Point(563, 42);
+            this.EnemyStatsTextbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EnemyStatsTextbox.Name = "EnemyStatsTextbox";
+            this.EnemyStatsTextbox.ReadOnly = true;
+            this.EnemyStatsTextbox.Size = new System.Drawing.Size(127, 204);
+            this.EnemyStatsTextbox.TabIndex = 6;
+            this.EnemyStatsTextbox.Text = "";
+            this.EnemyStatsTextbox.TextChanged += new System.EventHandler(this.EnemyStatsTextbox_TextChanged);
+            // 
+            // SelectEnemyDropDownList
+            // 
+            this.SelectEnemyDropDownList.FormattingEnabled = true;
+            this.SelectEnemyDropDownList.Location = new System.Drawing.Point(571, 263);
+            this.SelectEnemyDropDownList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SelectEnemyDropDownList.Name = "SelectEnemyDropDownList";
+            this.SelectEnemyDropDownList.Size = new System.Drawing.Size(106, 25);
+            this.SelectEnemyDropDownList.TabIndex = 7;
+            this.SelectEnemyDropDownList.SelectedIndexChanged += new System.EventHandler(this.SelectEnemy_SelectedIndexChanged);
+            // 
+            // attackEnemyButton
+            // 
+            this.attackEnemyButton.Location = new System.Drawing.Point(592, 310);
+            this.attackEnemyButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.attackEnemyButton.Name = "attackEnemyButton";
+            this.attackEnemyButton.Size = new System.Drawing.Size(66, 25);
+            this.attackEnemyButton.TabIndex = 8;
+            this.attackEnemyButton.Text = "Attack";
+            this.attackEnemyButton.UseVisualStyleBackColor = true;
+            this.attackEnemyButton.Click += new System.EventHandler(this.attackEnemyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 484);
+            this.Controls.Add(this.attackEnemyButton);
+            this.Controls.Add(this.SelectEnemyDropDownList);
+            this.Controls.Add(this.EnemyStatsTextbox);
+            this.Controls.Add(this.playerStatsLabel);
+            this.Controls.Add(this.mapDisplayTextBox);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.upButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.TextBox mapDisplayTextBox;
+        private System.Windows.Forms.Label playerStatsLabel;
+        private System.Windows.Forms.RichTextBox EnemyStatsTextbox;
+        private System.Windows.Forms.ComboBox SelectEnemyDropDownList;
+        private System.Windows.Forms.Button attackEnemyButton;
+    }
+}
+
---
+++ Semester-2-Poe-Part-2/Semester 2 POE Part 2/Semester 2 POE Part 1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semester_2_POE_Part_1
+{
+    public partial class Form1 : Form
+    {
+        GameEngine engine;  //declarations
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //creates game engine object, displays the map
+        {
+            engine = new GameEngine();            
+
+            DisplayMap();
+
+            engine.getMap().GetEnemies();
+
+            string[] entires = new string[engine.getMap().GetEnemies().Length]; //getting string array of eneimes on form load
+
+            for (int i = 0; i < entires.Length; i++) //drop down menu entries
+            {
+                entires[i] = engine.getMap().GetEnemies()[i].ToString();
+                SelectEnemyDropDownList.Items.Add(entires[i]); //adding entires to drop down menu on form load
+            }
+
+
+
+
+        }
+
+        private void EnemyStatsTextbox_TextChanged(object sender, EventArgs e)
+        {
+            //EnemyStatsTextbox.Text += "/n";
+            //EnemyStatsTextbox.Text += Enemy.ToString(enemyInfo);
+            
+        }
+
+        private void attackEnemyButton_Click(object sender, EventArgs e)
+        {
+            if(SelectEnemyDropDownList.SelectedIndex == null)
+            {
+                return; // to catch any errors if nothing in entered
+            }
+            try
+            {
+                if (engine.getMap().Heroprop.CheckRange(engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex]) == true)
+                {   //to attack an enemy if one is selected and in range
+                    engine.getMap().Heroprop.Attack(engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex]);
+                    EnemyStatsTextbox.Text = "Attack Success!\n";
+
+                    EnemyStatsTextbox.Text += engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex].ToString();
+                    if(engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex].isDead()== true)     //removes dead enemy symbols from the map and updates the map display
+                    {
+                        engine.getMap().GetMap()[engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex].X, engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex].Y] = new EmptyTile(engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex].X, engine.getMap().GetEnemies()[SelectEnemyDropDownList.SelectedIndex].Y, ". ");
+                        DisplayMap();
+                        engine.getMap().UpdateVision(engine.getMap().Heroprop);
+                        EnemyStatsTextbox.Text += "\nEnemy was killed";
+                    }
+                    //check if enemies are dead, if they are dead create a new array without the dead ones
+                    //set enemies method to overwrite the eneimes array + update combobox
+                }
+                else
+                {   // if no enemy is in range
+                    EnemyStatsTextbox.Text = "Attack Failed";
+                }
+            }
+            catch(IndexOutOfRangeException exception)
+            {   //if trying to attack an enemy thats dead
+                EnemyStatsTextbox.Text = "Enemy is unalived";
+            }
+            
+            //the code beneath here is to remove enemies from the current enemy array and drop down menu array if they die
+            int tmp = -1;
+
+            for (int i = 0; i < engine.getMap().GetEnemies().Length; i++)
+            {
+                if (engine.getMap().GetEnemies()[i].isDead() == true)
+                {
+                    tmp = i;
+                }
+            }
+
+            if(tmp != -1)
+            {
+                Enemy[] noDeadEnemies = new Enemy[engine.getMap().GetEnemies().Length -1 ];
+                int j = 0;
+                bool tempbool = true;
+                for(int i = 0; i < engine.getMap().GetEnemies().Length; i++)
+                {
+                    //add all elements into new array except the dead one
+                    if(j == tmp && tempbool == true)
+                    {
+                        tempbool = false;
+                        
+                        continue;
+                    }
+                    noDeadEnemies[j] = engine.getMap().GetEnemies()[i];
+                    j++;
+                }
+
+                engine.getMap().SetEnemies(noDeadEnemies); //update the array to be the living enemies
+
+                string[] entires = new string[engine.getMap().GetEnemies().Length]; //drop down menu entries
+
+                SelectEnemyDropDownList.Items.Clear();
+
+                for (int i = 0; i < entires.Length; i++)
+                {
+                    entires[i] = engine.getMap().GetEnemies()[i].ToString();
+                    SelectEnemyDropDownList.Items.Add(entires[i]);
+                }
+
+            }
+            //the code above here is to remove enemies from the current enemy array and drop down menu array if they die
+
+
+        }
+
+        private void SelectEnemy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //add iteams to the iteam array
+            //update ++ update on attack
+            //if (SelectEnemyDropDownList.Text == "Swamp Creature")
+            //{
+                //idk man f this
+            //}
+        }
+
+
+
+        private void DisplayMap()   //method to display map with symbols in a textbox
+        {
+            mapDisplayTextBox.Clear();
+            for (int i = 0; i < engine.getMap().GetMapHeight(); i++)
+            {
+                for (int j = 0; j < engine.getMap().GetMapWidth(); j++)
+                {
+                    mapDisplayTextBox.Text += engine.getMap().GetSymbol(i, j);
+                }
+                mapDisplayTextBox.Text += Environment.NewLine;
+            }
+
+            playerStatsLabel.Text = engine.getMap().Heroprop.ToString();
+        }
+
+        private void upButton_Click(object sender, EventArgs e) //moves hero up 
+        {
+            engine.MovePlayer(Character.movement.up);
+            engine.getMap().MapFill();
+            DisplayMap();
+        }
+
+        private void downButton_Click(object sender, EventArgs e)   //moves hero down
+        {
+            engine.MovePlayer(Character.movement.down);
+            engine.getMap().MapFill();      
+                                               
+            DisplayMap();
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)  //moves hero right
+        {
+            engine.MovePlayer(Character.movement.right);
+            engine.getMap().MapFill();
+            DisplayMap();
+        }
+        private void leftButton_Click(object sender, EventArgs e)   //moves hero left
+        {
+            engine.MovePlayer(Character.movement.left);
+            engine.getMap().MapFill();
+            DisplayMap();
+        }
+    }
+}
